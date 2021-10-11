e108d75072534349904d5982d0b0986f8768349d
Simon Cozens <simon@simon-cozens.org>
2021-10-11 09:33:56 +0100

    Normalize language tags

--- lib/fez/Routine.py
+++ lib/fez/Routine.py
@@ -69,7 +69,7 @@ class Routine(FEZVerb):
     def languages(self, args):
         rv = []
         while args:
-            rv.append(tuple(map(str,args[0:2])))
+            rv.append(tuple(map((lambda x: "%-4s" % x),args[0:2])))
             args = args[2:]
         return rv
 
