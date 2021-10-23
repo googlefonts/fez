2715d9f0b5e29ec2b1cc70153a27b4f6ed33cda1
Simon Cozens <simon@simon-cozens.org>
2021-10-23 09:15:49 +0100

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ config = {
     'long_description': open('README.md', 'r').read(),
     'long_description_content_type': 'text/markdown',
     'license': 'MIT',
-    'version': '1.1.0',
+    'version': '1.2.0',
     'install_requires': install_requires,
     'classifiers': [
         "Programming Language :: Python",
